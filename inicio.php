cc2fd08dceabfb57c2aed1e340bf468e7e03173b
Libee01 <145665352+Libee01@users.noreply.github.com>
2024-03-22 20:20:49 +0100

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PowerStream</title>
+    <link rel="stylesheet" href="css/inicio.css">
+</head>
+<body>
+    <header class="header">
+        <div class="menu-container">
+            <a href="#" class="logo">PowerSteam</a>
+            <input type="checkbox" id="menu">
+            <label for="menu">
+                <img src="img2/menu.png" class="menu-icono" alt="menu">
+            </label>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="#">Películas</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </nav>
+            <a href="#" class="btn-1">Login</a>
+        </div>
+        <div class="header-content container">
+            <div class="header-1">
+                <img src="img2/venom.png" alt="">
+                <a href="#" class="btn-2">Ver ahora</a>
+            </div>
+            <div class="header-2">
+                <h1>Las mejores <br> peliculas</h1>
+                <img src="img2/play.png" alt="">
+            </div>
+        </div>
+    </header>
+    <section class="movies container">
+        <h2>Peliculas más vistas</h2>
+        <hr>
+        <div class="box-container-1">
+            <div class="box-1">
+        </div>
+    </section>
+    <script src="js/inicio.js"></script>
+</body>
+</html>
