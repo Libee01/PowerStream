2919403bba2450ede3ebdf8135c1ae54fd0312db
Libee01 <145665352+Libee01@users.noreply.github.com>
2024-03-30 09:37:49 +0100

    Add files via upload

---
+++ mario/home.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/home.css">
+	<title>PowerStream</title>
+	
+</head>
+<body>
+	<header>
+		<div class="contenedor">
+			<h2 class="logotipo">PowerStream</h2>
+			<nav>
+				<a href="#" class="activo">Inicio</a>
+				<a href="#">Películas</a>
+				<a href="#">Series</a>
+				<a href="#">Más Recientes</a>
+				<a href="#">Mi lista</a>
+			</nav>
+		</div>
+	</header>
+
+	<main>
+		<div class="pelicula-principal0" id="pelicula0">
+			<div class="contenedor">
+				<h3 class="titulo">Interestellar</h3>
+				<p class="descripcion">
+					Narra las aventuras de un grupo de exploradores que hacen uso de un agujero de gusano recientemente descubierto para 
+					superar las limitaciones de los viajes espaciales tripulados y vencer las inmensas distancias que tiene un viaje interestelar.
+				</p>
+				<button id="video0" role="button" class="boton"><i class="fas fa-play"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-plus"></i>Añadir a mi lista</button>
+			</div>
+		</div>
+
+		<div class="pelicula-principal1" id="pelicula1">
+			<div class="contenedor">
+				<h3 class="titulo">Interestellar</h3>
+				<p class="descripcion">
+					Narra las aventuras de un grupo de exploradores que hacen uso de un agujero de gusano recientemente descubierto para 
+					superar las limitaciones de los viajes espaciales tripulados y vencer las inmensas distancias que tiene un viaje interestelar.
+				</p>
+				<button id="video1" role="button" class="boton"><i class="fas fa-play"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-plus"></i>Añadir a mi lista</button>
+			</div>
+		</div>
+
+		<div class="pelicula-principal2" id="pelicula2">
+			<div class="contenedor">
+				<h3 class="titulo">Marte (The Martian)</h3>
+				<p class="descripcion">
+				El astronauta Mark Watney, botánico e ingeniero mecánico, es dado por muerto y queda atrapado en el planeta Marte 
+				cuando la tripulación del Ares 3, a causa de una emergencia, debe evacuar su lugar de aterrizaje y regresar a la Tierra. 
+				Sin su tripulación, se ve obligado a sobrevivir y encontrar la forma de regresar a la Tierra, antes de que se le agoten los suministros. 
+				</p>
+				<button id="video2" role="button" class="boton"><i class="fas fa-play"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-plus"></i>Añadir a mi lista</button>
+			</div>
+		</div>
+
+		<div class="pelicula-principal3" id="pelicula3">
+			<div class="contenedor">
+				<h3 class="titulo">Ready Player One</h3>
+				<p class="descripcion">
+				Cuando el creador de una realidad virtual llamada OASIS muere, lanza un reto póstumo a todos sus usuarios para que 
+				busquen un huevo de pascua, que dará al que lo encuentre su fortuna y el control de su mundo.
+				</p>
+				<button id="video3" role="button" class="boton"><i class="fas fa-play"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-plus"></i>Añadir a mi lista</button>
+			</div>
+		</div>
+
+		<div class="pelicula-principal4" id="pelicula4">
+			<div class="contenedor">
+				<h3 class="titulo">Top gun: Ídolos del aire</h3>
+				<p class="descripcion">
+				La Marina de los Estados Unidos ha creado una escuela de élite para pilotos con el fin de sacar una promoción 
+				de expertos en técnicas de combate. En la academia, más conocida como Top Gun, a los mejores se les entrena 
+				para ser intrépidos y fríos al mismo tiempo, capaces de no perder los nervios en situaciones extremas y de 
+				no inmutarse al romper la barrera del sonido a los mandos de un F-14. A la escuela llega el joven Maverick, 
+				famoso por su temeraria aunque brillante forma de pilotar.
+				</p>
+				<button id="video4" role="button" class="boton"><i class="fas fa-play"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-plus"></i>Añadir a mi lista</button>
+			</div>
+		</div>
+
+		<div class="pelicula-principal5" id="pelicula5">
+			<div class="contenedor">
+				<h3 class="titulo">Tron</h3>
+				<p class="descripcion">
+				Cuando Sam Flynn (Garrett Hedlund), programador experto en tecnología de 27 años e hijo de Kevin Flynn
+				(Jeff Bridges), investiga la desaparición de su padre, se encuentra de repente inmerso en un peligroso 
+				y salvaje mundo surreal donde existen feroces luchas a muerte, un lugar paralelo donde su padre ha estado viviendo durante 25 años.
+				</p>
+				<button id="video5" role="button" class="boton"><i class="fas fa-play"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-plus"></i>Añadir a mi lista</button>
+			</div>
+		</div>
+
+		<div class="pelicula-principal6" id="pelicula6">
+			<div class="contenedor">
+				<h3 class="titulo">El caballero oscuro</h3>
+				<p class="descripcion">
+				Batman/Bruce Wayne regresa para continuar su guerra contra el crimen. Con la ayuda del teniente Jim Gordon y del Fiscal 
+				del Distrito Harvey Dent, Batman se propone destruir el crimen organizado en la ciudad de Gotham. El triunvirato 
+				demuestra su eficacia, pero, de repente, aparece Joker, un nuevo criminal que desencadena el caos y tiene aterrados a los ciudadanos.
+				</p>
+				<button id="video6" role="button" class="boton"><i class="fas fa-play"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-plus"></i>Añadir a mi lista</button>
+			</div>
+		</div>
+		
+		<div class="pelicula-principal7" id="pelicula7">
+			<div class="contenedor">
+				<h3 class="titulo">Infierno Blanco</h3>
+				<p class="descripcion">
+				En “Infierno blanco”, el personaje interpretado por Liam Neeson es el líder de un indisciplinado grupo de trabajadores de una 
+				refinería cuyo avión se estrella en las remotas montañas de Alaska. Los supervivientes, expuestos a heridas mortales y un tiempo 
+				inclemente, disponen de pocos días para escapar de los gélidos elementos.
+				</p>
+				<button id="video7" role="button" class="boton"><i class="fas fa-play"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-plus"></i>Añadir a mi lista</button>
+			</div>
+		</div>
+
+		<div class="peliculas-recomendadas contenedor">
+			<div class="contenedor-titulo-controles">
+				<h3>Películas Recomendadas</h3>
+				<div class="indicadores"></div>
+			</div>
+
+			<div class="contenedor-principal">
+				<button role="button" id="flecha-izquierda" class="flecha-izquierda"><i class="fas fa-angle-left"></i></button>
+
+				<div class="contenedor-carousel">
+					<div class="carousel">
+						<div class="pelicula">
+							<a href="#"><img id="img1" src="img/1.png" alt="" onclick="pulsar1()"></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img id="img2" src="img/2.png" alt="" onclick="pulsar2()"></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img id="img3" src="img/3.png" alt="" onclick="pulsar3()"></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img id="img4" src="img/4.png" alt="" onclick="pulsar4()"></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img id="img5" src="img/5.png" alt="" onclick="pulsar5()"></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img id="img6" src="img/6.png" alt="" onclick="pulsar6()"></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img id="img7" src="img/7.png" alt="" onclick="pulsar7()"></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/1.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/2.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/3.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/1.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/2.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/3.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/4.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/5.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/6.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/7.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/1.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/2.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/3.png" alt=""></a>
+						</div>
+						<div class="pelicula">
+							<a href="#"><img src="img/4.png" alt=""></a>
+						</div>
+					</div>
+				</div>
+
+				<button role="button" id="flecha-derecha" class="flecha-derecha"><i class="fas fa-angle-right"></i></button>
+			</div>
+		</div>
+		
+	</main>
+	
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	<script src="js/home.js"></script>
+</body>
+</html>
