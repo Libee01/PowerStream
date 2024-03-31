a35252aac363fb48875fbe673f084ca6f9056fda
Libee01 <145665352+Libee01@users.noreply.github.com>
2024-03-31 12:20:20 +0200

    Proyecto-ASIR-Mario

---
+++ ProyectoASIR - Mario/PowerStream.sql
@@ -0,0 +1,47 @@
+-- Base de datos de PowerStream
+drop database powerstream;
+
+create database powerstream if not exists;
+
+use powerstream;
+
+create table usuarios(
+    ID_usuario int auto_increment primary key,
+    email varchar(50) unique not null,
+    password varchar(255) not null,
+    plan varchar(20) not null,
+    fecha_suscripcion date not null,
+    fecha_renovacion date not null,
+    pagado boolean
+);
+
+create table videos(
+    ID_video int auto_increment primary key,
+    titulo varchar(255) unique not null,
+    video varchar(255) unique not null,
+    portada varchar(40) not null,
+    descripcion varchar(255) not null,
+    tipo varchar(40) not null,
+    plan varchar(20) references usuarios(plan),
+    genero varchar(50) not null,
+    fecha_subida date not null
+);
+
+create table lista(
+    ID_video int references prueba(id),
+    ID_usuario int references usuarios(ID_usuario),
+    titulo varchar(40) references prueba(titulo),
+    portada varchar(40) references prueba(portada),
+    categoria varchar(50) references prueba(categoria),
+    img_publi varchar(255) references prueba(img_publi)
+);
+
+CREATE TABLE prueba (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    portada VARCHAR(255) NOT NULL,
+    descripcion TEXT NOT NULL,
+    categoria varchar(50) NOT NULL,
+    img_publi varchar(255) NOT NULL,
+    nom_publi varchar(255)
+);
---
+++ ProyectoASIR - Mario/js/premium.js
@@ -0,0 +1,149 @@
+var slideIndex = 0;
+var slides = document.getElementsByClassName("slides")[0].getElementsByClassName("header-1");
+var interval;
+
+showSlides();
+
+function showSlides() {
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length - 1) {slideIndex = 0} // Reiniciar índice si se llega al final
+    
+    slides[slideIndex].style.display = "block";  
+
+    interval = setTimeout(showSlides, 5000); // Cambiar imagen cada 5 segundos
+}
+
+// Detener el deslizamiento automático cuando el puntero está sobre una imagen
+for (var i = 0; i < slides.length; i++) {
+    slides[i].addEventListener("mouseover", stopSlide);
+    slides[i].addEventListener("mouseout", startSlide);
+}
+
+function stopSlide() {
+    clearTimeout(interval);
+}
+
+function startSlide() {
+    interval = setTimeout(showSlides, 5000);
+}
+
+// Cargar más películas de acción
+var loadMoreBtn1 = document.querySelector('#load-more-1');
+var currentItem1 = 4;
+
+loadMoreBtn1.onclick = () => {
+    var boxes = [...document.querySelectorAll(
+        '.box-container-1 .box-1'
+    )];
+    for(var i = currentItem1; i < currentItem1 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem1 += 4;
+    if(currentItem1 >= boxes.length) {
+        loadMoreBtn1.style.display = 'none'
+    }
+}
+
+// Cargar más películas de drama
+var loadMoreBtn2 = document.querySelector('#load-more-2');
+var currentItem2 = 4;
+
+loadMoreBtn2.onclick = () => {
+    var boxes = [...document.querySelectorAll(
+        '.box-container-2 .box-2'
+    )];
+    for(var i = currentItem2; i < currentItem2 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem2 += 4;
+    if(currentItem2 >= boxes.length) {
+        loadMoreBtn2.style.display = 'none'
+    }
+}
+
+// Cargar más películas de terror
+var loadMoreBtn3 = document.querySelector('#load-more-3');
+var currentItem3 = 4;
+
+loadMoreBtn3.onclick = () => {
+    var boxes = [...document.querySelectorAll(
+        '.box-container-3 .box-3'
+    )];
+    for(var i = currentItem3; i < currentItem3 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem3 += 4;
+    if(currentItem3 >= boxes.length) {
+        loadMoreBtn3.style.display = 'none'
+    }
+}
+
+// Cargar más películas románticas
+var loadMoreBtn4 = document.querySelector('#load-more-4');
+var currentItem4 = 4;
+
+loadMoreBtn4.onclick = () => {
+    var boxes = [...document.querySelectorAll(
+        '.box-container-4 .box-4'
+    )];
+    for(var i = currentItem4; i < currentItem4 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem4 += 4;
+    if(currentItem4 >= boxes.length) {
+        loadMoreBtn4.style.display = 'none'
+    }
+}
+
+// Cargar más películas de comedia
+var loadMoreBtn5 = document.querySelector('#load-more-5');
+var currentItem5 = 4;
+
+loadMoreBtn5.onclick = () => {
+    var boxes = [...document.querySelectorAll(
+        '.box-container-5 .box-5'
+    )];
+    for(var i = currentItem5; i < currentItem5 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem5 += 4;
+    if(currentItem5 >= boxes.length) {
+        loadMoreBtn5.style.display = 'none'
+    }
+}
+
+// Cargar más películas de aventuras
+var loadMoreBtn6 = document.querySelector('#load-more-6');
+var currentItem6 = 4;
+
+loadMoreBtn6.onclick = () => {
+    var boxes = [...document.querySelectorAll(
+        '.box-container-6 .box-6'
+    )];
+    for(var i = currentItem6; i < currentItem6 + 4; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem6 += 4;
+    if(currentItem6 >= boxes.length) {
+        loadMoreBtn6.style.display = 'none'
+    }
+}
+
+
+function añadirALista(id, id_usuario, titulo, portada, categoria, img_publi) {
+    // Realizar una solicitud al servidor para añadir la película a la lista de reproducción
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "guardar_pelicula.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(xhr.responseText); // Mostrar la respuesta del servidor (opcional)
+        }
+    };
+    // Enviar datos a guardar_pelicula.php
+    xhr.send("id=" + id + "&id_usuario=" + id_usuario + "&titulo=" + titulo + "&portada=" + portada + "&categoria=" + categoria + "&img_publi=" + img_publi);
+}
