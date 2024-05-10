667e7ff000a0ce3c67dbce0337baad1fe23c5b06
JAndres721 <145665395+JAndres721@users.noreply.github.com>
2024-05-10 20:36:35 +0200

    Nueva base de datos

--- PowerStream.sql
+++ PowerStream.sql
@@ -6,7 +6,7 @@ create database powerstream if not exists;
 use powerstream;
 
 create table usuarios(
-    ID_usuario int auto_increment primary key,
+    id_usuario int auto_increment primary key,
     email varchar(50) unique not null,
     password varchar(30) not null,
     plan varchar(20) not null,
@@ -16,39 +16,30 @@ create table usuarios(
 );
 
 create table videos(
-    ID_video int auto_increment primary key,
+    id_video int auto_increment primary key,
     titulo varchar(255) unique not null,
-    video varchar(255) unique not null,
-    portada varchar(40) not null,
     descripcion varchar(255) not null,
     tipo varchar(40) not null,
-    plan varchar(20) references usuarios(plan),
     genero varchar(50) not null,
-    fecha_subida date not null
-);
-
-create table lista(
-    ID_video int references videos(ID_video),
-    ID_usuario int references usuarios(ID_usuario),
-    titulo varchar(40) references videos(titulo)
-);
-
-CREATE TABLE prueba (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(100) NOT NULL,
-    portada VARCHAR(255) NOT NULL,
-    tipo varchar(255) not null,
-    descripcion TEXT NOT NULL,
-    categoria varchar(50) NOT NULL,
+    fecha_subida date not null,
+    video varchar(255) unique not null,
+    img_portada varchar(40) not null,   
     img_publi varchar(255) NOT NULL,
     nom_publi varchar(255) NOT NUlL
 );
+
+create table lista(
+    id_video int references videos(id_video),
+    id_usuario int references usuarios(id_usuario),
+    titulo varchar(40) references videos(titulo)
+);
+
 
 DELIMITER //
 
 CREATE PROCEDURE prueba1()
 BEGIN
-    SELECT id, titulo, portada, LEFT(descripcion, 5), categoria, img_publi, nom_publi, tipo FROM prueba;
+    SELECT id_video, titulo,LEFT(descripcion, 5), tipo, genero, fecha_subida, video, img_portada, img_publi, nom_publi, FROM videos;
 END //
 
 DELIMITER ;
